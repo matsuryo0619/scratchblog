054fff9bbbff02debc7fe86b11696c6205a6c31b
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-01-13 21:35:30 +0900

    Delete cursor.js

--- cursor.js
+++
@@ -1,9 +0,0 @@
-const img = document.getElementById("cursor");
-
-document.addEventListener("mousemove", (event) => {
-  const mouseX = event.pageX;
-  const mouseY = event.pageY;
-  
-  img.style.left = mouseX + "px";
-  img.style.top = mouseY + "px";
-});
